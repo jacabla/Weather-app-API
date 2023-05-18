09b94f1123202bd3d99f9baa9d9ee7ead3cb4c37
Jason <castrob.jason@gmail.com>
2023-05-18 11:06:58 -0600

    create components and fetch API

---
+++ src/components/search.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+
+export default function Search() {
+  return (
+    <>
+      <TextField
+        id="filled-basic"
+        label="Buscar ciudad"
+        className="input"
+        variant="filled"
+      />
+    </>
+  );
+}
